73b8df97c601dad3bef63452d8f95e214de86955
David Gillespie <david.gillespie@digital.cabinet-office.gov.uk>
2026-07-23 09:18:49 +0100

    add name field to DataSource

--- src/dia/sources/known.py
+++ src/dia/sources/known.py
@@ -5,12 +5,14 @@ from dia.types import DataSource
 
 KNOWN_SOURCES: dict[str, DataSource] = {
     "gats-business-cases": DataSource(
+        name="gats-business-cases",
         document_type=DocumentType.BUSINESS_CASE,
         bucket="c-af-get-approval-spend-service-business-cases",
         prefix="files/",
         cross_account=True,
     ),
     "sr-bids-2025": DataSource(
+        name="sr-bids-2025",
         document_type=DocumentType.SR_BIDS,
         bucket="gds-idea-sr-query-documentstore-dev",
         prefix="",
